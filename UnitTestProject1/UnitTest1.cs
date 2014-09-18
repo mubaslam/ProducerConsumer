175eb4256ee66b36cda51833049a7c50b1f00ecf
bjarnesvanberg <bjsv@easj.dk>
2014-09-18 09:37:26 +0200

    Created Project

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            BoundedBuffer buf = new BoundedBuffer(4);
+
+            Producer prod = new Producer(buf, 10);
+            Consumer con = new Consumer(buf);
+
+            Parallel.Invoke(prod.Run, con.Run);
+        }
+    }
+}
